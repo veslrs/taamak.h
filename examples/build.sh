41ccbf754acb6ebf420c422b01e3f89d0db24a93
veslrs <sub.fang.zeyuan@outlook.com>
2026-09-04 20:04:11 +0200

    updated build.sh

--- examples/build.sh
+++ examples/build.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-set -euxo pipefail
+set -xe
 
-gcc -O3 -Wall -Wextra -pedantic \
-    -I. -I/opt/homebrew/opt/openblas/include -I/usr/local/lib \
-    -L/opt/homebrew/opt/openblas/lib -L/usr/local/lib \
-    -lopenblas -Wl,-rpath,/opt/homebrew/opt/openblas/lib \
-    -lnlopt -lm -Wl,-rpath,/usr/local/lib \
-    elt.c \
-    -o elt
+if [ "$1" = "-D" ]
+then 
+    CFLAGS="-Wall -Wextra -O0 -g -fsanitize=address,undefined"
+elif [ $1 = "-R" ]
+then
+    CFLAGS="-Wall -Wextra -O2"
+else
+    echo "[ERROR] 1st argument: -D for debug build; -R for release build."
+    exit -1
+fi
 
-gcc -O3 -Wall -Wextra -pedantic \
-    -I. -I/opt/homebrew/opt/openblas/include -I/usr/local/lib \
-    -L/opt/homebrew/opt/openblas/lib -L/usr/local/lib \
-    -lopenblas -Wl,-rpath,/opt/homebrew/opt/openblas/lib \
-    -lnlopt -lm -Wl,-rpath,/usr/local/lib \
-    esa.c \
-    -o esa
+gcc $CFLAGS \
+    `pkg-config --cflags openblas` \
+    -o $2 \
+    $2.c \
+    -L$HOME/opt/openblas/lib \
+    -l:libopenblas.a -lm -lpthread
